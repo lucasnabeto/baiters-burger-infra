6fa1e8dfb1f5224a1fbff68e70bf701ae1e7db06
Lucas Nabeto <lucas.nabeto@outlook.com>
2025-09-16 20:54:36 -0300

    env: create Terraform files and modules

---
+++ main.tf
@@ -0,0 +1,19 @@
+module "api-gateway" {
+  source = "./api-gateway"
+}
+
+module "cognito" {
+  source = "./cognito"
+}
+
+module "eks" {
+  source = "./eks"
+}
+
+module "lambda" {
+  source = "./lambda"
+}
+
+module "rds" {
+  source = "./rds"
+}
